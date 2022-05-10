070783687433287414f11a75023e4d2265d82bca
Wahaha <b0464221@gmail.com>
2022-05-10 15:36:19 +0700

    Add 'Toggle Theme Button'

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -10,9 +10,13 @@ export const ThemeContext = createContext();
       dark : { syntax: '#ddd', ui: '#333', bg: '#555' }
     } 
 
+    toggleTheme = () => {
+      this.setState ({ isLightTheme: !this.state.isLightTheme });  
+    }
+
     render() { 
      return (
-        <ThemeContext.Provider value={{...this.state}}>
+        <ThemeContext.Provider value={{...this.state, toggleTheme: this.toggleTheme}}>
           { this.props.children }
         </ThemeContext.Provider>
      );
